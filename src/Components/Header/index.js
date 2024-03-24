62f6e753337e73a2a7602ca69a91ef2fb99e0a25
RishikeshPolasa <rishipolasa@gmail.com>
2024-03-24 16:23:42 +0530

    feat/boiler code

---
+++ src/Components/Header/index.js
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+function index() {
+  return (
+    <div className="flex flex-col">
+      <div className="LogogWrapper text-3xl">header</div>
+      login
+    </div>
+  );
+}
+
+export default index;
